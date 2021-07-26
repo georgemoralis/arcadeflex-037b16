94d337c455d2c383008a363f1864c607844cfaf3
chusogar <chusogar@gmail.com>
2021-07-26 10:00:11 +0200

    commando tilemaps are perfect! (vulgus still needs to be fixed)

--- emulator/src/main/java/gr/codebb/arcadeflex/WIP/v037b16/mame/tilemapC.java
+++ emulator/src/main/java/gr/codebb/arcadeflex/WIP/v037b16/mame/tilemapC.java
@@ -1331,6 +1331,7 @@ public static void tilemap_set_clip(struct_tilemap tilemap, rectangle clip) {
             int /*UINT32*/ flags,
             int pitch )
     {
+        //System.out.println("draw_color_mask");
         
     	IntArray pPenToPixel = new IntArray(tilemap.pPenToPixel[flags&(TILE_SWAPXY|TILE_FLIPY|TILE_FLIPX)]);
         pPenToPixel.offset=0;
@@ -1451,6 +1452,7 @@ public static void tilemap_set_clip(struct_tilemap tilemap, rectangle clip) {
     	int flags,
     	int pitch )
     {
+        //System.out.println("draw_mask");
     	IntArray pPenToPixel = new IntArray(tilemap.pPenToPixel[flags&(TILE_SWAPXY|TILE_FLIPY|TILE_FLIPX)]);
         pPenToPixel.offset=0;
     	int tx,ty;
@@ -1462,7 +1464,7 @@ public static void tilemap_set_clip(struct_tilemap tilemap, rectangle clip) {
     	{
     		for( ty=tile_size; ty!=0; ty-- )
     		{
-    			pSource = pPenData;
+    			pSource = new UBytePtr(pPenData);
     			for( tx=tile_size/2; tx!=0; tx-- )
     			{
     				data = pSource.readinc();
@@ -1486,7 +1488,7 @@ public static void tilemap_set_clip(struct_tilemap tilemap, rectangle clip) {
     	{
     		for( ty=tile_size; ty!=0; ty-- )
     		{
-    			pSource = pPenData;
+    			pSource = new UBytePtr(pPenData);
     			for( tx=tile_size; tx!=0; tx-- )
     			{
     				data = pSource.readinc();
@@ -2653,10 +2655,67 @@ public static void tilemap_set_clip(struct_tilemap tilemap, rectangle clip) {
 /*TODO*///#endif /* DECLARE */
 /*TODO*///    
     
-    public static DrawTileHandlerPtr draw_tile8x8x8BPP = new DrawTileHandlerPtr() {
+    public static DrawTileHandlerPtr draw_tile8x8x8BPP  = new DrawTileHandlerPtr() {
         @Override
         public void handler(struct_tilemap tilemap, int cached_indx, int col, int row) {
-            
+            //System.out.println("draw_tile8x8x8BPP");
+                cached_tile_info cached_tile_info = tilemap.cached_tile_info[cached_indx];
+		int /*UINT32*/ tile_size = tilemap.tile_size;
+		UBytePtr pPenData = new UBytePtr(cached_tile_info.pen_data);
+		int pitch = tile_size + cached_tile_info.skip;
+		IntArray pPalData = new IntArray(cached_tile_info.pal_data);
+                //pPalData.offset=0;
+		int /*UINT32*/ flags = cached_tile_info.u32_flags;
+		int /*UINT32*/ y0 = tile_size*row;
+		int /*UINT32*/ x0 = tile_size*col;
+		osd_bitmap pPixmap = tilemap.pixmap;
+		IntArray pPenToPixel = new IntArray(tilemap.pPenToPixel[flags&(TILE_SWAPXY|TILE_FLIPY|TILE_FLIPX)]);
+                pPenToPixel.offset=0;
+		int tx;
+		int ty;
+		UBytePtr pSource;
+		int /*UINT8*/ data;
+		int /*UINT32*/ yx;
+	
+		if ((flags & TILE_4BPP) != 0)
+		{
+			for( ty=tile_size; ty!=0; ty-- )
+			{
+				pSource = new UBytePtr(pPenData);
+				for( tx=tile_size/2; tx!=0; tx-- )
+				{
+					data = pSource.readinc();
+					yx = pPenToPixel.read(); pPenToPixel.offset++;
+					//*(x0+(yx%MAX_TILESIZE)+(DATA_TYPE *)pPixmap.line[y0+yx/MAX_TILESIZE]) = pPalData[data&0xf];
+                                        pPixmap.line[y0+yx/MAX_TILESIZE].write(x0+(yx%MAX_TILESIZE), pPalData.read(data&0xf));
+					yx = pPenToPixel.read(); pPenToPixel.offset++;
+					//*(x0+(yx%MAX_TILESIZE)+(DATA_TYPE *)pPixmap.line[y0+yx/MAX_TILESIZE]) = pPalData[data>>4];
+                                        pPixmap.line[y0+yx/MAX_TILESIZE].write(x0+(yx%MAX_TILESIZE) , pPalData.read(data>>4));
+				}
+				pPenData.offset += pitch/2;
+			}
+		}
+		else
+		{
+			for( ty=tile_size; ty!=0; ty-- )
+			{
+				pSource = new UBytePtr(pPenData);
+				for( tx=tile_size; tx!=0; tx-- )
+				{
+					data = pSource.readinc();
+					yx = pPenToPixel.read(); pPenToPixel.offset++;
+					//*(x0+(yx%MAX_TILESIZE)+(DATA_TYPE *)pPixmap.line[y0+yx/MAX_TILESIZE]) = pPalData[data];
+                                        pPixmap.line[y0+yx/MAX_TILESIZE].write(x0+(yx%MAX_TILESIZE), pPalData.read(data));
+				}
+				pPenData.offset += pitch;
+			}
+		}
+    }};
+
+    public static DrawTileHandlerPtr old_draw_tile8x8x8BPP = new DrawTileHandlerPtr() {
+        @Override
+        public void handler(struct_tilemap tilemap, int cached_indx, int col, int row) {
+            //System.out.println("draw_tile8x8x8BPP");
             cached_tile_info cached_tile_info = tilemap.cached_tile_info[cached_indx];
             int /*UINT32*/ tile_size = tilemap.tile_size;
             UBytePtr pPenData = new UBytePtr(cached_tile_info.pen_data);
@@ -2678,7 +2737,7 @@ public static void tilemap_set_clip(struct_tilemap tilemap, rectangle clip) {
 
             if(( flags&TILE_4BPP ) != 0)
             {
-                System.out.println("1");
+                //System.out.println("1");
                     for( ty=tile_size; ty!=0; ty-- )
                     {
                             pSource = pPenData;
@@ -2898,7 +2957,8 @@ public static void tilemap_set_clip(struct_tilemap tilemap, rectangle clip) {
     
     public static DrawHandlerPtr draw8x8x8BPP = new DrawHandlerPtr() {
         @Override
-        public void handler(int xpos, int ypos) {      
+        public void handler(int xpos, int ypos) {  
+            //System.out.println("draw8x8x8BPP");
              generic8draw(xpos, ypos, 8, 8);
         }
     };
@@ -3406,7 +3466,8 @@ public static void tilemap_set_clip(struct_tilemap tilemap, rectangle clip) {
     
     public static DrawHandlerPtr draw32x32x8BPP = new DrawHandlerPtr() {
         @Override
-        public void handler(int xpos, int ypos) {      
+        public void handler(int xpos, int ypos) {     
+            //System.out.println("draw32x32x8BPP");
             generic8draw(xpos, ypos, 32, 32);
         }
     };
